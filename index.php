f0274e7914824345abf209c69a9116c744d177b0
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-21 15:19:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// variabili
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Censored text</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
